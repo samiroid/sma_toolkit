c1e2d809daf05a09c2c93ae00f52e1d038191a8d
Silvio Amir <silvio.aam@gmail.com>
2017-04-09 17:04:42 -0400

    refactored embedding loading methods

--- embeddings.py
+++ embeddings.py
@@ -37,33 +37,62 @@ class Emb_Mapper():
         with open(path,"wb") as fod:
             cPickle.dump(self.W.get_value(), fod, cPickle.HIGHEST_PROTOCOL)
 
-def read_embeddings(path):
+# def embeddings_to_dict(path, max_words=None):
+#     """
+#         Read word embeddings into a dictionary
+#     """
+#     w2v = {}
+#     data = None
+#     with codecs.open(path,"r","utf-8") as fid:
+#         fid.readline()        
+#         data = fid.readlines()
+#     #avoid extra comparisons if we want load all the words
+#     if max_words is None:
+#         for line in data.split("\n"):
+#             entry = line.split()
+#             if len(entry) > 2:
+#                 w2v[entry[0]] = np.array(entry[1:]).astype('float32')
+#     else:
+#         for i, line in enumerate(data.split("\n")):
+#             entry = line.split()
+#             if len(entry) > 2:
+#                 w2v[entry[0]] = np.array(entry[1:]).astype('float32')
+#             if i >= max_words:break
+#     return w2v   
 
-    w2v = embeddings_to_dict(path)    
-    emb_size = w2v.values()[0].shape[0]
-    n_items  = len(w2v) 
-    E = np.zeros((emb_size, n_items))
-    wrd2idx = {w:i for i,w in enumerate(w2v.keys())}
-    for w,i in wrd2idx.items():
-        E[:,i] = w2v[w]
-    
-    return E, wrd2idx
-
-def get_embeddings(path, wrd2idx):
-
+def embeddings_to_dict(path, max_words=None):
     """
-        Recover an embedding matrix consisting of the relevant
-        vectors for the given set of words
+        Read word embeddings into a dictionary
     """
+    w2v = {}    
     with codecs.open(path,"r","utf-8") as fid:
-        voc_size = len(wrd2idx)        
-        _, emb_size = fid.readline().split()        
-        E = np.zeros((int(emb_size), voc_size))
-        for line in fid.readlines():
-            items = line.split()
-            wrd   = items[0]
-            if wrd in wrd2idx:
-                E[:, wrd2idx[wrd]] = np.array(items[1:]).astype(float)
+        #ignore first line
+        fid.readline()                
+        #avoid extra comparisons if we want load all the words
+        if max_words is None:
+            for line in fid:
+                entry = line.split()
+                if len(entry) > 2:
+                    w2v[entry[0]] = np.array(entry[1:]).astype('float32')
+        else:
+            for i, line in enumerate(fid):
+                entry = line.split()
+                if len(entry) > 2:
+                    w2v[entry[0]] = np.array(entry[1:]).astype('float32')
+                if i >= max_words:break
+    return w2v 
+
+def read_embeddings(path, wrd2idx=None, max_words=None):
+
+    w2v = embeddings_to_dict(path,max_words)        
+    #if no word index is specified read all the embedding vocabulary
+    if wrd2idx is None:
+        wrd2idx = {w:i for i,w in enumerate(w2v.keys())}                
+    #build embedding matrix
+    emb_size = w2v.values()[0].shape[0]    
+    E = np.zeros((emb_size, len(wrd2idx)))    
+    for w,i in wrd2idx.items():
+        if w in w2v: E[:,i] = w2v[w]            
     # Number of out of embedding vocabulary embeddings
     n_OOEV = np.sum((E.sum(0) == 0).astype(int))
     perc = n_OOEV*100./len(wrd2idx)
@@ -73,8 +102,8 @@ def get_embeddings(path, wrd2idx):
     ooev_idx = np.where(~E.any(axis=0))[0]
     idx2wrd = idx_2_wrd(wrd2idx)
     ooevs = [idx2wrd[idx] for idx in ooev_idx]
-
-    return E, ooevs
+    
+    return E, ooevs, wrd2idx
 
 def filter_embeddings(path_in, path_out, wrd2idx):
 
@@ -82,43 +111,113 @@ def filter_embeddings(path_in, path_out, wrd2idx):
         Filter embeddings file to contain only the relevant set
         of words (so that it can be loaded faster)
     """
-    emb_values = []
+
+    w2v = embeddings_to_dict(path_in)  
     ooevs = wrd2idx.copy()
     with codecs.open(path_out,"w","utf-8") as fod:
-        with codecs.open(path_in,"r","utf-8") as fid:
-            voc_size = len(wrd2idx)
-            _, emb_size = fid.readline().split()        
-            # emb_values = np.zeros(int(emb_size))
-            fod.write(str(voc_size)+"\t"+str(emb_size)+"\n")
-            for line in fid.readlines():
-                items = line.split()
-                wrd   = items[0]
-                if wrd in wrd2idx:
-                    del ooevs[wrd]
-                    emb_values.append(np.array(items[1:]).astype(float))
-                    fod.write(line)
+        voc_size = len(wrd2idx)
+        emb_size = w2v.values()[0].shape[0]   
+        fod.write(str(voc_size)+"\t"+str(emb_size)+"\n")
+        for word,emb in w2v.items():
+            if word in wrd2idx:
+                del ooevs[word]                
+                fod.write("%s %s\n" % (word, " ".join(map(str, emb))))
         perc = len(ooevs)*100./len(wrd2idx)
         print ("%d/%d (%2.2f %%) words in vocabulary found no embedding" 
            % (len(ooevs), len(wrd2idx), perc)) 
         #ooev words
         return ooevs
 
-def save_embeddings(path, E, wrd2idx):
+def save_txt(path, E, wrd2idx):
     with open(path,"w") as fod:
         fod.write("%d %d\n" % (E.shape[1],E.shape[0]))  
         for word, idx in wrd2idx.items():      
             emb = E[:,idx]
             fod.write("%s %s\n" % (word, " ".join(map(str, emb))))
 
-def embeddings_to_dict(path):
-    """
-        Read word embeddings into a dictionary
-    """
-    w2v = {}
-    with codecs.open(path,"r","utf-8") as fid:
-        fid.readline()        
-        for line in fid:
-            entry = line.split()
-            if len(entry) > 2:
-                w2v[entry[0]] = np.array(entry[1:]).astype('float32')
-    return w2v   
+# def filter_embeddings(path_in, path_out, wrd2idx):
+
+#     """
+#         Filter embeddings file to contain only the relevant set
+#         of words (so that it can be loaded faster)
+#     """
+#     emb_values = []
+#     ooevs = wrd2idx.copy()
+#     with codecs.open(path_out,"w","utf-8") as fod:
+#         with codecs.open(path_in,"r","utf-8") as fid:
+#             voc_size = len(wrd2idx)
+#             _, emb_size = fid.readline().split()        
+#             # emb_values = np.zeros(int(emb_size))
+#             fod.write(str(voc_size)+"\t"+str(emb_size)+"\n")
+#             for line in fid.readlines():
+#                 items = line.split()
+#                 wrd   = items[0]
+#                 if wrd in wrd2idx:
+#                     del ooevs[wrd]
+#                     emb_values.append(np.array(items[1:]).astype(float))
+#                     fod.write(line)
+#         perc = len(ooevs)*100./len(wrd2idx)
+#         print ("%d/%d (%2.2f %%) words in vocabulary found no embedding" 
+#            % (len(ooevs), len(wrd2idx), perc)) 
+#         #ooev words
+#         return ooevs
+
+# def get_embeddings(path, wrd2idx, max_words):
+
+#     """
+#         Recover an embedding matrix consisting of the relevant
+#         vectors for the given set of words
+#     """
+#     with codecs.open(path,"r","utf-8") as fid:
+#         voc_size = len(wrd2idx)        
+#         _, emb_size = fid.readline().split()        
+#         E = np.zeros((int(emb_size), voc_size))
+#         for line in fid.readlines():
+#             items = line.split()
+#             if len(items) > 2: 
+#                 wrd   = items[0]
+#                 if wrd in wrd2idx:
+#                     E[:, wrd2idx[wrd]] = np.array(items[1:]).astype(float)
+#     # Number of out of embedding vocabulary embeddings
+#     n_OOEV = np.sum((E.sum(0) == 0).astype(int))
+#     perc = n_OOEV*100./len(wrd2idx)
+#     print ("%d/%d (%2.2f %%) words in vocabulary found no embedding" 
+#            % (n_OOEV, len(wrd2idx), perc)) 
+
+#     ooev_idx = np.where(~E.any(axis=0))[0]
+#     idx2wrd = idx_2_wrd(wrd2idx)
+#     ooevs = [idx2wrd[idx] for idx in ooev_idx]
+
+#     return E, ooevs
+
+# def get_embeddings(path, wrd2idx, max_words):
+
+#     """
+#         Recover an embedding matrix consisting of the relevant
+#         vectors for the given set of words
+#     """
+#     with codecs.open(path,"r","utf-8") as fid:
+#         voc_size = len(wrd2idx)        
+#         _, emb_size = fid.readline().split()        
+#         E = np.zeros((int(emb_size), voc_size))
+#         for line in fid.readlines():
+#             items = line.split()
+#             if len(items) > 2: 
+#                 wrd   = items[0]
+#                 if wrd in wrd2idx:
+#                     E[:, wrd2idx[wrd]] = np.array(items[1:]).astype(float)
+#     # Number of out of embedding vocabulary embeddings
+#     n_OOEV = np.sum((E.sum(0) == 0).astype(int))
+#     perc = n_OOEV*100./len(wrd2idx)
+#     print ("%d/%d (%2.2f %%) words in vocabulary found no embedding" 
+#            % (n_OOEV, len(wrd2idx), perc)) 
+
+#     ooev_idx = np.where(~E.any(axis=0))[0]
+#     idx2wrd = idx_2_wrd(wrd2idx)
+#     ooevs = [idx2wrd[idx] for idx in ooev_idx]
+
+#     return E, ooevs
+
+
+
+
